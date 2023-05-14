1dceeace131e776c2ea53c93eccafba91dcd0c3c
Psoume <lorraine.vinel@gmail.com>
2023-05-14 12:09:10 +0200

    XML Parsers

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
-$xml=simplexml_load_file("./XML/03 - AT_14-5_17-2279.xml") or die("Error: Cannot create object");
-echo $xml->Avis_Technique_Ventilation . "<br>";
-echo $xml->Titre_AT . "<br>";
-echo $xml->Titulaire . "<br>";
-echo $xml->Code_Titulaire;
-echo "coucou";
+// EXEMPLE AVEC SIMPLE XML
+// $xml=simplexml_load_file("./XML/03 - AT_14-5_17-2279.xml") or die("Error: Cannot create object");
+// echo $xml->Avis_Technique_Ventilation . "<br>";
+// echo $xml->Titre_AT . "<br>";
+// echo $xml->Titulaire . "<br>";
+// echo $xml->Liste_AT_Ancien->Num_AT_Ancien;
+// echo "coucou";
+
+// AVEX XML READER
+// $xml = new XMLReader();
+// $xml->open("./XML/03 - AT_14-5_17-2279.xml");
+
+// while ($xml->read()) {
+//     if ($xml->name == "Titulaire") {
+//         print $xml->readInnerXML() ;
+//         $xml->next();
+//     }
+// }
 ?> 
