f09304e8adb2bc1ac8bb9d72ca6e74cd662663cd
Psum <lorraine.vinel@orange.fr>
2023-05-22 11:00:02 +0200

    JS create fields

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Choix du fichier XML parmi ceux disponibles + chargement
 function chooseXML(at){
     window.location.href = ("?AT="+at);
 };
@@ -15,9 +16,17 @@ function loadXML(at){
     xhr.onload = function(){
         if (xhr.responseXML != null)
         {   
-            var xml = xhr.responseXML;
-            console.log(xml.getElementsByTagName('Titre_AT')[0].textContent);
-                // document.getElementById("Titre_AT").value=xml.getElementsByTagName('Titre_AT')[0].textContent;
+            var xml = xhr.responseXML; // le fichier XML choisi
+            console.log(xml.getElementsByTagName('Titre_AT')[0].textContent); //comment on accède aux éléments
+            loadFields(xml,'text',['Titre_AT','Titulaire','Code_Titulaire','Industriel','Code_Industriel','Num_AT','Num_AT_Ancien[]'],'list-generalites');
+            addEmptyDiv('NumATAncienField','list-generalites');
+            addButton("addEmptyField('Num_AT_Ancien[]','text','NumATAncienField')",'+','list-generalites');
+            loadFields(xml,'text',['Date_Application','Date_Fin_Application','Usage_EA'],'list-generalites');
+            addText('Usages :','list-generalites');
+            loadFields(xml,'checkbox',['Individuel','Collectif','Hotel'],'list-generalites');
+            addText('Caractéristiques :','list-generalites');
+            loadFields(xml,'checkbox',['Double_Flux','Autoreglable','Hygroreglable','Basse_Pression'],'list-generalites');
+            addField(xml,'Type_extraction','text','Type_extraction','list-generalites')
                 // document.getElementById("Titulaire").value=xml.getElementsByTagName('Titulaire')[0].textContent;
                 // document.getElementById("Code_Titulaire").value=xml.getElementsByTagName('Code_Titulaire')[0].textContent;
                 // document.getElementById("Industriel").value=xml.getElementsByTagName('Industriel')[0].textContent;
@@ -25,53 +34,89 @@ function loadXML(at){
                 // document.getElementById("Num_AT").value=xml.getElementsByTagName('Num_AT')[0].textContent; 
             };
         }
-
     };
     
 
+    function loadFields(xml,type, fields,divId){
+        fields.forEach(element => addField(xml,element,type,element,divId));
+    }
 
+    function addField(xml,name,type,label,divId) //crée un input dans un container id
+    {
+        var lab = document.createElement("label");
+        var input = document.createElement("input");  
+        
+        if(typeof(xml.getElementsByTagName(name)[0])!=='undefined' && xml.getElementsByTagName(name)[0]!==null)
+            {var value = xml.getElementsByTagName(name)[0].textContent;}
+        else{var value ="";}
 
-//Pre-remplissage des champs si on lui fournit un fichier XML
+        input.type = type;
+        input.name = name;
+        input.id = name;
+        lab.innerHTML = label;
+        lab.htmlFor = name;
 
-// var form = document.getElementById("XML_Form");
+        switch(type){
+            case 'text':
+                input.value = value;
+                break;
+            case 'checkbox':
+                lab.htmlclass ='form-check-label' ;
+                input.htmlclass = 'form-check-input';
+                switch(value){
+                    case 'true':
+                        input.checked = true;
+                        break;
+                    case 'false':
+                        input.checked = false;
+                        break;
+                }
+                input.value = name;
+                break;
+        }
+        
+        
+        var container = document.getElementById(divId);
+        container.appendChild(lab);
+        container.appendChild(input);
+    };
 
-// form.addEventListener("submit", function(event) {
-//     event.preventDefault(); 
+    function addEmptyField(name,type,divId) //crée un input dans un container id
+    {
+        var input = document.createElement("input");   
 
-//     var fileInput = document.getElementById("Xml_File");
-//     var file = fileInput.files[0];
+        input.type = type;
+        input.name = name;
+        input.id = name;
+        var container = document.getElementById(divId);
+        container.appendChild(input);
+    };
+    
+    function addEmptyDiv(id,divId){
+        var div = document.createElement("div"); 
+        div.id = id;
+        var container = document.getElementById(divId);
+        container.appendChild(div);
+    };
 
-//     if (file) {
-//     var reader = new FileReader();
-//     reader.onload = function(e) {
-//         var xmlContent = e.target.result;
-//         const parser = new DOMParser();
-//         xml = parser.parseFromString(xmlContent, "application/xml").documentElement;
-//         document.getElementById("Titre_AT").value=xml.getElementsByTagName('Titre_AT')[0].textContent;
-//         document.getElementById("Titulaire").value=xml.getElementsByTagName('Titulaire')[0].textContent;
-//         document.getElementById("Code_Titulaire").value=xml.getElementsByTagName('Code_Titulaire')[0].textContent;
-//         document.getElementById("Industriel").value=xml.getElementsByTagName('Industriel')[0].textContent;
-//         document.getElementById("Code_Industriel").value=xml.getElementsByTagName('Code_Industriel')[0].textContent;
-//         document.getElementById("Num_AT").value=xml.getElementsByTagName('Num_AT')[0].textContent;
+    function addText(content,divId){
+        var p = document.createElement('p');
+        var text = document.createTextNode(content);
+        p.appendChild(text);
+        var container = document.getElementById(divId);
+        container.appendChild(p);
+    };
 
-//         };
-//     reader.readAsText(file);
-//     }
-// });
-
-/////////////////////////////////////
-
-function addField(name,type,lab,id) //crée un input dans un container id
+    function addButton(onclick,content,divId)
 {
-    var input = document.createElement("input");   
-    var label = document.createElement("label");
-    input.type = type;
-    input.name = name;
-    label.innerHTML = lab;
-    label.htmlFor = name;
-    var container = document.getElementById(id);
-    container.appendChild(label);
-    container.appendChild(input);
+    var button = document.createElement("button"); 
+    button.type = "button";
+    button.href = '#';
+    button.innerHTML = content;
+    button.className = "btn btn-primary";
+    button.setAttribute("onclick", onclick)
+    var container = document.getElementById(divId);
+    container.appendChild(button);
 };
 
 function addCheckbox(name,lab,id) //crée un input dans un container id
@@ -90,73 +135,64 @@ function addCheckbox(name,lab,id) //crée un input dans un container id
 };
 
 
-function loadConfig(id) 
-{
-    // On crée le container pour le modalContent
-    var container = document.getElementById(id);
-    // On rajoute le reste du contenu
-    fetch("config.html")
-    .then(response => response.text())
-    .then(contenu => {
-        container.innerHTML = contenu;
-        console.log(contenu);
-    })
-    .catch(error => {
-        console.log("Erreur : " + error);
-    });
-}
+// function loadConfig(id) 
+// {
+//     // On crée le container pour le modalContent
+//     var container = document.getElementById(id);
+//     // On rajoute le reste du contenu
+//     fetch("config.html")
+//     .then(response => response.text())
+//     .then(contenu => {
+//         container.innerHTML = contenu;
+//         console.log(contenu);
+//     })
+//     .catch(error => {
+//         console.log("Erreur : " + error);
+//     });
+// }
 
 
-function addModal(id) // id étant le nom du container dans lequel on crée le bouton modal (ex : ConfigsAT1)
-{
-    var container = document.getElementById(id);
-    var NbrConfigs = container.childElementCount+1;
+// function addModal(id) // id étant le nom du container dans lequel on crée le bouton modal (ex : ConfigsAT1)
+// {
+//     var container = document.getElementById(id);
+//     var NbrConfigs = container.childElementCount+1;
 
-    // On crée la div ConfigsAT1_1
-    var newDiv = document.createElement("div");
-    var newDivId = id+"_"+NbrConfigs; //newDivId = ConfigsAT1_1
-    newDiv.className = "modalConfig";
-    newDiv.id = newDivId;
-    container.appendChild(newDiv);
+//     // On crée la div ConfigsAT1_1
+//     var newDiv = document.createElement("div");
+//     var newDivId = id+"_"+NbrConfigs; //newDivId = ConfigsAT1_1
+//     newDiv.className = "modalConfig";
+//     newDiv.id = newDivId;
+//     container.appendChild(newDiv);
 
-    // On crée le bouton dans ConfigsAT1_1
-    var button = document.createElement("button"); 
-    button.type = "button";
-    button.innerHTML = "Config"+NbrConfigs;
-    button.className = "btn btn-primary";
-    button.setAttribute("data-bs-toggle", "modal");
-    button.setAttribute("data-bs-target", "#Modal_"+newDivId );
-    newDiv.appendChild(button);
+//     // On crée le bouton dans ConfigsAT1_1
+//     var button = document.createElement("button"); 
+//     button.type = "button";
+//     button.innerHTML = "Config"+NbrConfigs;
+//     button.className = "btn btn-primary";
+//     button.setAttribute("data-bs-toggle", "modal");
+//     button.setAttribute("data-bs-target", "#Modal_"+newDivId );
+//     newDiv.appendChild(button);
 
-    // On crée le modal à la suite du bouton
-    // On crée d'abord la div qui va contenir le modal
+//     // On crée le modal à la suite du bouton
+//     // On crée d'abord la div qui va contenir le modal
 
-    var divModalContent = document.createElement("div");
-    divModalContent.className = "modal fade";
-    divModalContent.id = "Modal_"+newDivId;
-    divModalContent.setAttribute("tabindex","-1");
-    divModalContent.setAttribute("aria-labelledby","Modal_"+newDivId+"Label");
-    divModalContent.setAttribute("aria-hidden","true");
-    newDiv.appendChild(divModalContent);
-    newDivModal = document.createElement("div");
+//     var divModalContent = document.createElement("div");
+//     divModalContent.className = "modal fade";
+//     divModalContent.id = "Modal_"+newDivId;
+//     divModalContent.setAttribute("tabindex","-1");
+//     divModalContent.setAttribute("aria-labelledby","Modal_"+newDivId+"Label");
+//     divModalContent.setAttribute("aria-hidden","true");
+//     newDiv.appendChild(divModalContent);
+//     newDivModal = document.createElement("div");
 
 
-    loadConfig("Modal_"+newDivId);
-    // D'abord on charge le modal puis on modifie le contenu
+//     loadConfig("Modal_"+newDivId);
+//     // D'abord on charge le modal puis on modifie le contenu
 
     
-};
+// };
 
-function addButton(description,onclick,id)
-{
-    var button = document.createElement("button"); 
-    button.type = "button";
-    button.innerHTML = description;
-    button.className = "btn btn-primary";
-    button.setAttribute("onclick", onclick)
-    var container = document.getElementById(id);
-    container.appendChild(button);
-};
+
 
 /////////////////////////////////////
 
@@ -217,3 +253,30 @@ function addButton(description,onclick,id)
 
 
 /////////////////////////////////////
+//Pre-remplissage des champs si on lui fournit un fichier XML
+
+// var form = document.getElementById("XML_Form");
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault(); 
+
+//     var fileInput = document.getElementById("Xml_File");
+//     var file = fileInput.files[0];
+
+//     if (file) {
+//     var reader = new FileReader();
+//     reader.onload = function(e) {
+//         var xmlContent = e.target.result;
+//         const parser = new DOMParser();
+//         xml = parser.parseFromString(xmlContent, "application/xml").documentElement;
+//         document.getElementById("Titre_AT").value=xml.getElementsByTagName('Titre_AT')[0].textContent;
+//         document.getElementById("Titulaire").value=xml.getElementsByTagName('Titulaire')[0].textContent;
+//         document.getElementById("Code_Titulaire").value=xml.getElementsByTagName('Code_Titulaire')[0].textContent;
+//         document.getElementById("Industriel").value=xml.getElementsByTagName('Industriel')[0].textContent;
+//         document.getElementById("Code_Industriel").value=xml.getElementsByTagName('Code_Industriel')[0].textContent;
+//         document.getElementById("Num_AT").value=xml.getElementsByTagName('Num_AT')[0].textContent;
+
+//         };
+//     reader.readAsText(file);
+//     }
+// });
--- view/form.php
+++ view/form.php
@@ -2,104 +2,40 @@
     <form method='post' action='./writeXML.php'>
         <div class="container">
             <div class="nav">
-                <div class="list-group" id="list-tab" role="tablist">
-                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Généralités</a>
-                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">ATS</a>
-                    <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Équipements</a>
-                    <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Puissances des extracteurs</a>
+                <div class="list-group" id="list-tab">
+                    <a class="list-group-item list-group-item-action active" id="list-generalites-list" data-bs-toggle="list" href="#list-generalites" aria-controls="list-generalites">Généralités</a>
+                    <a class="list-group-item list-group-item-action" id="list-ats-list" data-bs-toggle="list" href="#list-ats" aria-controls="list-ats">ATS</a>
+                    <a class="list-group-item list-group-item-action" id="list-equipements-list" data-bs-toggle="list" href="#list-equipements" aria-controls="list-equipements">Équipements</a>
+                    <a class="list-group-item list-group-item-action" id="list-puissance-extracteurs-list" data-bs-toggle="list" href="#list-puissance-extracteurs" aria-controls="list-puissance-extracteurs">Puissances des extracteurs</a>
                 </div>
             </div>
 
             <div class="content">
                 <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-                        
-                        <label for="Titre_AT">Titre_AT:</label></br>
-                        <input type='text' name="Titre_AT" id="Titre_AT" value='titre' /></br>
-                        <label for="Titulaire">Titulaire:</label></br>
-                        <input type='text' name="Titulaire" id="Titulaire" /></br>
-                        <label for="Code_Titulaire">Code_Titulaire:</label></br>
-                        <input type='text' name="Code_Titulaire" id="Code_Titulaire" /></br>
-                        <label for="Industriel">Industriel:</label></br>
-                        <input type='text' name="Industriel" id="Industriel" /></br>
-                        <label for="Code_Industriel">Code_Industriel:</label></br>
-                        <input type='text' name="Code_Industriel" id="Code_Industriel" /></br>
-                        <label for="Num_AT">Num_AT:</label></br>
-                        <input type='text' name="Num_AT" id="Num_AT" /></br>
-
-                        <label for="Num_AT_Ancien">Num_AT_Anciens :</label><br/>    
-                        <input type='text' name="Num_AT_Ancien[]" id="Num_AT_Ancien" /></br>
-                        <div id='NumATAncienField'></div>
-                        <button type='button' href='#'  onclick="addField('Num_AT_Ancien[]','text','','NumATAncienField')">+</button></br>
-
-                        <label for="Date_Application">Date_Application:</label></br>
-                        <input type='text' name="Date_Application" id="Date_Application" /></br>
-                        <label for="Date_Fin_Application">Date_Fin_Application:</label></br>
-                        <input type='text' name="Date_Fin_Application" id="Date_Fin_Application" /></br>
-                        <label for="Usage_EA">Usage_EA:</label></br>
-                        <input type='text' name="Usage_EA" id="Usage_EA" /></br>
-                        <p>Usages :</p>
-                        <label class="form-check-label" for="Individuel">Individuel :</label>
-                        <input class="form-check-input" type="checkbox" value="Individuel" name="Individuel">
-                        
-                        <label class="form-check-label" for="Collectif">Collectif :</label>
-                        <input class="form-check-input" type="checkbox" value='Collectif' name="Collectif">
-                        
-                        <label class="form-check-label" for="Hotel">Hotel :</label>
-                        <input class="form-check-input" type="checkbox" value="Hotel" name="Hotel">
-                        <br/>
-
-                        <p>Caractéristiques :</p>
-                        <label class="form-check-label" for="Double_Flux">Double_Flux :</label>
-                        <input class="form-check-input" type="checkbox" value="Double_Flux" name="Double_Flux">
-                        
-                        <label class="form-check-label" for="Autoreglable">Autoréglable :</label>
-                        <input class="form-check-input" type="checkbox" value="Autoreglable" name="Autoreglable">
-
-                        <label class="form-check-label" for="Hygroreglable">Hygroréglable :</label>
-                        <input class="form-check-input" type="checkbox" value="Hygroreglable" name="Hygroreglable">
-
-                        <label class="form-check-label" for="Basse_Pression">Basse_Pression :</label>
-                        <input class="form-check-input" type="checkbox" value="Basse_Pression" name="Basse_Pression">
-<br/>
-                        <label for="Type_extraction">Type_extraction:</label></br>
-                        <input type='text' name="Type_extraction" /></br>
-                        <label for="NB_AT">NB_AT:</label></br>
-                        <input type='number' name="NB_AT" id="NB_AT" /></br>
+                    <!-- GENERALITES -->
+                    <div class="tab-pane fade show active" id="list-generalites" aria-labelledby="list-generalites-list">
                     </div>
-                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-                        <div id='ATS'>
-                        </div>
-                        
-                        
-                        <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-                        
+                    <!-- ATS -->
+                    <div class="tab-pane fade" id="list-ats" aria-labelledby="list-ats-list">
+                      <!-- ATS NAV -->
+                      <ul class="nav nav-pills">
+                      <li class="nav-item">
+                        <button class="nav-link active" id="at_1-tab" data-bs-toggle="tab" data-bs-target="#at_1-tab-pane" type="button" aria-controls="at_1-tab-pane" aria-selected="true">At_1</button>
+                      </li>
+                      <li class="nav-item">
+                        <button class="nav-link" id="addAT-tab" data-bs-toggle="tab" data-bs-target="#addAT-tab-pane" type="button" aria-controls="addAT-tab-pane" aria-selected="false">+</button>
+                      </li>
+                      </ul>  
+                      <!-- ATS CONTENT -->
+                      <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="at_1-tab-pane" aria-labelledby="at_1-tab" tabindex="0">...</div>
+                        <div class="tab-pane fade" id="addAT-tab-pane" aria-labelledby="addAT-tab" tabindex="0">...</div>
+                      </div>
                     </div>
-                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+                    <!-- EQUIPEMENTS -->
+                    <div class="tab-pane fade" id="list-equipements" aria-labelledby="list-equipements-list">...</div>
+                    <!-- PUISSANCES EXTRACTEURS -->
+                    <div class="tab-pane fade" id="list-puissance-extracteurs" aria-labelledby="list-puissance-extracteurs-list">...</div>
                 </div>
             </div>
         </div>
